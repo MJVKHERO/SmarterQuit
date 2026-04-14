f8c073c58024be2047259be45147981d55bf3238
MJVKHERO <michieljespervanklaveren@gmail.com>
2026-04-14 07:41:34 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './pages/App'
 import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
 import Admin from './pages/Admin'
+import Calc from './pages/Calc'
 import Privacy from './pages/Privacy'
 import Terms from './pages/Terms'
 import Refund from './pages/Refund'
@@ -15,15 +16,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/app" element={<App />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/"        element={<Landing />} />
+        <Route path="/app"     element={<App />} />
+        <Route path="/calc"    element={<Calc />} />
+        <Route path="/blog"    element={<Blog />} />
         <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin"   element={<Admin />} />
         <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/refund" element={<Refund />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/terms"   element={<Terms />} />
+        <Route path="/refund"  element={<Refund />} />
+        <Route path="*"        element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
